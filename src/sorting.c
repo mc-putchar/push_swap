88bc84a0be0680d9beeb1af95987ee19b986be07
Martin Cutura <mcutura@student.42berlin.de>
2023-06-10 17:50:35 +0200

    added sorting

---
+++ src/sorting.c
@@ -0,0 +1,77 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sorting.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mcutura <mcutura@student.42berlin.de>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/10 16:46:05 by mcutura           #+#    #+#             */
+/*   Updated: 2023/06/10 17:46:33 by mcutura          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+int	is_sorted(t_roll *roll)
+{
+	t_roll	*start;
+
+	if (!roll)
+		return (1);
+	start = roll->next;
+	while (start != roll)
+	{
+		if (start->value < start->prev->value)
+			return (EXIT_FAILURE);
+		start = start->next;
+	}
+	return (EXIT_SUCCESS);
+}
+
+void	quicksort(int *tab, int size)
+{
+	int	pivot;
+	int	i;
+	int	j;
+
+	if (size < 2)
+		return ;
+	pivot = tab[size / 2];
+	i = -1;
+	j = size;
+	while (1)
+	{
+		while (tab[++i] < pivot)
+			;
+		while (tab[--j] > pivot)
+			;
+		if (i >= j)
+			break ;
+		ft_swap(&tab[i], &tab[j]);
+	}
+	quicksort(tab, i);
+	quicksort(tab + i, size - i);
+}
+
+int	*get_sorted_array(t_roll *stack_a, int *size)
+{
+	int		i;
+	int		*tab;
+	t_roll	*tmp;
+
+	if (!stack_a || !size)
+		return (NULL);
+	*size = roll_size(stack_a);
+	tab = malloc(sizeof(int) * *size);
+	if (!tab)
+		return (NULL);
+	i = 0;
+	tmp = stack_a;
+	while (i < *size)
+	{
+		tab[i++] = tmp->value;
+		tmp = tmp->next;
+	}
+	quicksort(tab, *size);
+	return (tab);
+}
